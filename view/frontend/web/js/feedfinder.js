21674a3aa7ede2ddcc8438a31f25833386c4d4fd
steve cantlay <steve.cantlay@gmail.com>
2016-03-01 22:28:43 +0000

    Updates

--- view/frontend/web/js/feedfinder.js
+++ view/frontend/web/js/feedfinder.js
@@ -8,11 +8,30 @@ define([
         options: {
         },
         _create: function() {
+            this.options.xml = this._convertXml(this.options.xml);
             console.log(this.options.xml);
         },
         _init: function() {
 
+        },
+        _convertXml: function(xml){
+            try {
+                var xmlDoc = $.parseXML(xml);
+            } catch (err) {
+                console.log(err);
+            }
+            return xmlDoc;
+        },
+        _setOption: function( key, value ) {
+            if ( key === "xml" ) {
+                value = this._convertXml( value );
+            }
+            this._super( key, value );
+        },
+        _setOptions: function( options ) {
+            this._super( options );
         }
+
     });
 
     return $.feedfinder.js;
